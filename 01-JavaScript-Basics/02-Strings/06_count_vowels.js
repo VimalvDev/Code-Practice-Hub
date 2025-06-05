bdc50f8124d00f3c4bf78490144ac81648b7c58a
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:25:18 +0530

    Update 06_count_vowels.js

--- 01-JavaScript-Basics/02-Strings/06_count_vowels.js
+++ 01-JavaScript-Basics/02-Strings/06_count_vowels.js
@@ -1,2 +1,35 @@
-//Count vowels in a string	
+//Count vowels in a string
 //Input: "hello" → Output: 2
+
+function way1(string) {
+  let splitTxt;
+  let count = 0;
+  splitTxt = string.toLowerCase().split("");
+
+  for (let i = 0; i < splitTxt.length; i++) {
+    if (
+      (splitTxt[i] == "a") ||
+      (splitTxt[i] == "e") ||
+      (splitTxt[i] == "i") ||
+      (splitTxt[i] == "o") ||
+      (splitTxt[i] == "u")
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+// console.log(way1("hEllo"))
+
+function way2(string) {
+  let count = 0;
+  let vowels = "aeiou";
+    let str = string.toLowerCase()
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(way2("hellOo"));
