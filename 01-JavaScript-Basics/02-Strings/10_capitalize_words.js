d50ee3f5a5fa8368c3c84d271efdd1dd5574c337
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:26:50 +0530

    Update 10_capitalize_words.js

--- 01-JavaScript-Basics/02-Strings/10_capitalize_words.js
+++ 01-JavaScript-Basics/02-Strings/10_capitalize_words.js
@@ -1,2 +1,13 @@
-//Capitalize first letter of each word	
+//Capitalize first letter of each word
 //Input: "hello world" → Output: "Hello World"
+
+
+function way1(string) {
+  let splitedString = string.split(" ");
+    for (let i = 0; i < splitedString.length; i++) {
+        splitedString[i] = splitedString[i][0].toUpperCase() + splitedString[i].slice(1).toLowerCase()
+    }
+    return splitedString.join(" ")
+}
+
+console.log(way1("hello world"));
