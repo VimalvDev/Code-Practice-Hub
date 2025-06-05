b9b6c324b889ab6c0d3d09b40453222d9baa8242
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:27:26 +0530

    Update 11_is_even_odd.js

--- 01-JavaScript-Basics/03-Functions-Conditionals/11_is_even_odd.js
+++ 01-JavaScript-Basics/03-Functions-Conditionals/11_is_even_odd.js
@@ -1,2 +1,9 @@
-//Write a function to check even or odd	
+//Write a function to check even or odd
 //Input: 5 → Output: "Odd"
+
+function evenOdd(num) {
+  if (num % 2 === 0) return `Even`;
+  else return `Odd`;
+}
+
+console.log(evenOdd(98))
