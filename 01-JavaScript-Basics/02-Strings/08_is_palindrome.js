eabacd32549dad9be9a98051f90a708dbc40e3f1
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:26:14 +0530

    Update 08_is_palindrome.js

--- 01-JavaScript-Basics/02-Strings/08_is_palindrome.js
+++ 01-JavaScript-Basics/02-Strings/08_is_palindrome.js
@@ -1,2 +1,35 @@
-//Check if a string is a palindrome	
+//Check if a string is a palindrome
 //Input: "madam" → Output: true
+
+function way1(string) {
+  //Array method
+  let orignalString = string.toLowerCase();
+  let reverseString = [];
+  //Reversing the string
+  for (let i = 0; i < string.length; i++) {
+    reverseString.unshift(string[i]);
+  }
+  if (orignalString == reverseString.join("")) {
+    return `The string is palindrome`;
+  } else {
+    return `The string isn't palindrome`;
+  }
+}
+// console.log(way1("aba"));
+
+//String method
+function way2(string) {
+    
+  let str = string.toLowerCase();
+  let reverseString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
+  if (str === reverseString) {
+    return `The string is palindrome`;
+  } else {
+    return `The string isn't palindrome`;
+  }
+}
+console.log(way2("abA"));
