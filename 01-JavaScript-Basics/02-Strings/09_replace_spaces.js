ecd6fd976168ea170c7cb6279ac092f71ff4fc77
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:26:30 +0530

    Update 09_replace_spaces.js

--- 01-JavaScript-Basics/02-Strings/09_replace_spaces.js
+++ 01-JavaScript-Basics/02-Strings/09_replace_spaces.js
@@ -1,2 +1,28 @@
-//Replace all spaces with `-`	
+//Replace all spaces with `-`
 //Input: "a b c" → Output: "a-b-c"
+
+function way1(string) {
+  let replacedString = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == " ") {
+      replacedString += "-";
+    } else {
+      replacedString += string[i];
+    }
+    }
+    return replacedString
+}
+
+// console.log(way1("hell o m y nam e"))
+
+function way2(string){
+  return string.split(" ").join("-")
+}
+console.log(way2("hell o m y nam e"))
+
+function way3(string)
+{
+  return string.replaceAll(" ","-")
+}
+console.log(way3("hell o m y nam e"))
