9c1c359bb0dd8f372236ec085e44ddd856de4932
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:25:32 +0530

    Update 07_count-vowels.js

--- 01-JavaScript-Basics/02-Strings/07_count-vowels.js
+++ 01-JavaScript-Basics/02-Strings/07_count-vowels.js
@@ -1,2 +1,20 @@
-//Reverse a string manually
+//Reverse a string
 //Input: "cat" → Output: "tac"
+
+function way1(string) {
+    let reverseString = [];
+    //dont need to split string because we are already accessing each character with string[i]
+    for (let i = 0; i < string.length;i++)
+    {
+        reverseString.unshift(string[i])
+    }
+    return reverseString.join("")
+}
+console.log(way1("HeLoLo"))
+
+function way2(string)
+{
+    //Need to split because we are accessing whole string, not each character unlike way1
+    return string.split("").reverse().join("")
+}
+console.log(way2("Developer"))
