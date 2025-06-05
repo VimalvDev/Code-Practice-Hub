a0ab637ae724d17b34f19204c24feb651f7bf970
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 00:27:45 +0530

    Update 12_is_prime.js

--- 01-JavaScript-Basics/03-Functions-Conditionals/12_is_prime.js
+++ 01-JavaScript-Basics/03-Functions-Conditionals/12_is_prime.js
@@ -1,2 +1,19 @@
-//Function to check if a number is prime	
+//Function to check if a number is prime
 //Input: 7 → Output: true
+
+function prime(num)
+{
+    if (num <= 1)
+        return `Not prime`
+    
+    for (let i = 0; i <= num;i++)
+    {
+        if (num % i === 0)
+        {
+            return false
+        } 
+        
+    }
+    return true
+}
+console.log(prime(10))
