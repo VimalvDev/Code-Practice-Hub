0d4d344b39d166e1fcacb50e4ced450c6ebd4693
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 17:04:02 +0530

    Update 15_max_of_two.js

--- 01-JavaScript-Basics/03-Functions-Conditionals/15_max_of_two.js
+++ 01-JavaScript-Basics/03-Functions-Conditionals/15_max_of_two.js
@@ -1,2 +1,12 @@
-//Return the greater of 2 numbers	
+//Return the greater of 2 numbers
 //Input: 10, 20 → Output: 20
+
+function maxOf(num) {
+    let currentNum = num[0];
+    let nextNum = num[1];
+
+    if (currentNum > nextNum) return currentNum;
+    else return nextNum;
+}
+
+console.log(maxOf([10,20]))
