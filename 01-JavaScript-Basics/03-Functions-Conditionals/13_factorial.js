42d0c72740c21f829cfc746d6b6aeaadc8a469a7
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 17:02:18 +0530

    Update 13_factorial.js

--- 01-JavaScript-Basics/03-Functions-Conditionals/13_factorial.js
+++ 01-JavaScript-Basics/03-Functions-Conditionals/13_factorial.js
@@ -1,2 +1,15 @@
-//Function to return factorial of number	
+//Function to return factorial of number
 //Input: 5 → Output: 120
+
+function factorial(num)
+{
+    //Factorial of 0! is 1, so the result will always be result>=1, also if we multiply something with 0, the result will be zero which we dont want
+
+    let result = 1;
+    for (let i = 1; i <= num;i++)
+    {
+        result*=i
+    }
+    return result
+}
+console.log(factorial(3))
