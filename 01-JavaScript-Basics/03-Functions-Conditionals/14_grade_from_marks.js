0706e6bab4442e7de8e7e119789a449eb873db01
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2025-06-06 17:02:40 +0530

    Update 14_grade_from_marks.js

--- 01-JavaScript-Basics/03-Functions-Conditionals/14_grade_from_marks.js
+++ 01-JavaScript-Basics/03-Functions-Conditionals/14_grade_from_marks.js
@@ -1,2 +1,11 @@
-//Find the grade (A/B/C) from marks	
+//Find the grade (A/B/C) from marks
 //Input: 85 → Output: "A"
+
+function grade(marks) {
+  if (marks >= 95) return `A+`;
+  else if (marks >= 85) return `A`;
+  else if (marks >= 60) return `B`;
+  else if (marks >= 40) return `C`;
+  else return `F`;
+}
+console.log(grade(60))
